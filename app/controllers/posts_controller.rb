916f32e3665694489800e5d8fa57870fbaa1943e
Snurnicyn Konstantin <ks_on_v@mail.ru>
2016-04-12 19:19:46 +0600

    Create Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+  #before_action :set_cart, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    flash[:error] = nil
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(@post.user_id)
+  end
+
+  def create
+    @post = Post.new(title: params[:post][:title],
+                     text:  params[:post][:text],
+                     user_id: current_user.id)
+    if @post.save
+      render 'show'
+    else
+      flash[:error] = 'Впопрос и пописание не должны быть пустыми и меньше двух символов.'
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if Post.update_post(@post, params[:post])
+      render 'show'
+    else
+      flash[:error] = 'Впопрос и пописание не должны быть пустыми и меньше двух символов.'
+      render 'edit'
+    end
+  end
+
+end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
-class Mark < ActiveRecord::Base
+class Tag < ActiveRecord::Base
   has_and_belongs_to_many :posts
   has_and_belongs_to_many :users
 end
