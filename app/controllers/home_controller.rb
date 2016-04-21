fa872a3cac1b1375799e55e165ed5d78ef25e0ea
Snurnicyn Konstantin <ks_on_v@mail.ru>
2016-04-21 10:34:16 +0600

    will_paginate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
     if params[:without_answer] == 'true'
-      @posts = Post.uncommented.decorate
+      @posts = Post.paginate(page: params[:page], per_page: 10).recent.uncommented.decorate
     else
-      @posts = Post.recent.decorate
+      @posts = Post.paginate(page: params[:page], per_page: 10).recent.decorate
+      #Post.paginate(:page => params[:page], :per_page => 30)
     end
   end
 end
