3964c485416e23bcea320dd954b705f0be59d84a
Snurnicyn Konstantin <ks_on_v@mail.ru>
2016-04-26 10:35:51 +0600

    graphics

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,4 +5,14 @@ class UserDecorator < Draper::Decorator
     object.created_at.strftime("%H:%M %d-%m-%Y")
   end
 
+  def statistic_activity
+    tags = []
+    object.posts.each do |post|
+      tags << post.tags.first
+    end
+    tag_count = {}
+    tags.group_by(&:tag_name).each { |k, v| tag_count[k] = v.length }
+    tag_count
+  end
+
 end
